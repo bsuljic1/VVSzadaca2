3db782ef60c0aa17fdb85c1ef71a8659dc97c5cb
asiljak1 <asiljak1@etf.unsa.ba>
2020-12-01 12:47:16 +0100

    validacija za clana

--- eParking/Testovi za zadatak 1/UnitTestClan.cs
+++ eParking/Testovi za zadatak 1/UnitTestClan.cs
@@ -10,12 +10,63 @@ namespace Testovi_za_zadatak_1
     {
         static Clan clan;
         static Lokacija lokacija1;
+        static IEnumerable<object[]> ValidData
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { "user1", "password12", "Adresa", "Automobil", "123-A-123", 5, 2022, 5, 1 },
+                    new object[] { "user1", "12password", "Adresa", "Automobil", "123-A-123", 5, 2022, 5, 1 },
+                    new object[] { "user1", "passworddd", "Adresa", "Automobil", "123-A-123", 5, 2022, 5, 1 },
+                    new object[] { "user1", "0123456789", "Adresa", "Automobil", "123-A-123", 5, 2022, 5, 1 },
+                    new object[] { "user1", "pass123word", "Adresa", "Automobil", "123-A-123", 5, 2022, 5, 1 }
+                };
+            }
+        }
+        static IEnumerable<object[]> InvalidData
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { "user1", "", "Adresa", "Automobil", "123-A-123", 5, 2022, 5, 1 },
+                    new object[] { "user1", "pass", "Adresa", "Automobil", "123-A-123", 5, 2022, 5, 1 },
+                    new object[] { "user1", "password12/", "Adresa", "Automobil", "123-A-123", 5, 2022, 5, 1 },
+                    new object[] { "user1", "123/abc/123", "Adresa", "Automobil", "123-A-123", 5, 2022, 5, 1 },
+                    new object[] { "user1", " 123 123 ", "Adresa", "Automobil", "123-A-123", 5, 2022, 5, 1 }
+                };
+            }
+        }
+
         [ClassInitialize()]
         public static void Inicijalizacija(TestContext context)
         {
             clan = new Clan("Lamija", "1234567891a", "lamija@etf.unsa.ba", new Vozilo("Motor", "123-E-123", 1), new DateTime(2021, 10, 10));
             List<string> listaUlica = new List<string> { "Vrbas naselje" };
             lokacija1 = new Lokacija("Bugojno", listaUlica, 2, 100);
+        }
+
+        /// <summary>
+        /// Student bi se trebao ispravno kreirati - provjera da li se u konstruktoru došlo
+        /// do posljednje linije gdje se status postavlja na aktivan
+        /// </summary>
+        [TestMethod]
+        [DynamicData("ValidData")]
+        public void TestValidnihPodataka(string user, string pass, string address, string vehicleType, string vehiclePlates, int vehicleNOfSeats, int endDateYear, int endDateMonth, int endDateDay)
+        {
+            Clan clan1 = new Clan(user, pass, address, new Vozilo(vehicleType, vehiclePlates, vehicleNOfSeats), DateTime.Now.AddYears(1));
+            Assert.AreEqual(clan1.Status, Status.Aktivna);
+        }
+        /// <summary>
+        /// Student se ne bi trebao kreirati - setter za password bi trebao bacati izuzetak
+        /// </summary>
+        [TestMethod]
+        [DynamicData("InvalidData")]
+        [ExpectedException(typeof(FormatException))]
+        public void TestNevalidnihPodataka(string user, string pass, string address, string vehicleType, string vehiclePlates, int vehicleNOfSeats, int endDateYear, int endDateMonth, int endDateDay)
+        {
+            Clan clan1 = new Clan(user, pass, address, new Vozilo(vehicleType, vehiclePlates, vehicleNOfSeats), DateTime.Now.AddYears(1));
         }
         /// <summary>
         /// Testira get metode
@@ -31,16 +82,6 @@ namespace Testovi_za_zadatak_1
             Assert.AreEqual(new DateTime(2021, 10, 10), aktivnaDo1);
         }
         /// <summary>
-        /// Testira neispravan format passworda i seter
-        /// </summary>
-        [TestMethod]
-        [ExpectedException(typeof(FormatException))]
-        public void TestNeispravanFormat()
-        {
-            Clan clan1 = new Clan("Lamija", "1a2a1a1a1a1a1", "lamija@etf.unsa.ba", new Vozilo("Motor", "123-E-123", 1), new DateTime(2021, 10, 10));
-                clan1.Password = "123a";   
-        }
-        /// <summary>
         /// Testira otkazivanje rezervacije
         /// </summary>
         [TestMethod]
@@ -51,5 +92,6 @@ namespace Testovi_za_zadatak_1
             clan.OtkažiRezervaciju();
             Assert.IsNull(clan.RezervisanoParkingMjesto);
         }
+       
     }
 }
